0f23546bfb93965b94088c64bbf0b6888dc13382
Stefan Buhrmester <stefan@buhrmi.de>
2025-06-02 23:27:14 +0700

    update test

--- dummy/test/system/livestores_test.rb
+++ dummy/test/system/livestores_test.rb
@@ -4,17 +4,15 @@ class LivestoreTest < ApplicationSystemTestCase
 
   test 'subscribing, updating, appending' do
     visit '/messages'
-    assert_text 'henlo'
-    assert_text "Object value: old"
-    assert_text "deleteMe"
+    assert_text 'old1old3'
+    assert_text "123456"
+    assert_text "henlo"
+    assert_text "foundme"
     click_on 'Make Message'
-    assert_text 'Hello World'
+    assert_no_text "foundme"
+    assert_text "gotcha"
     assert_text 'newnew'
-    assert_text 'Large Number: 1000'
-    assert_text 'word: hellochunk2'
-    assert_text "Object value: Hello World"
-    assert_text "keepMe"
-    assert_no_text "deleteMe"
+    
 
   end
 end
